24802b9f6129a1911ce6abd777d8b225f398763a
0resuto <pilot.ua4@gmail.com>
2023-03-24 21:33:37 +0300

    1

--- VoiceRecognation/record_mic.py
+++ VoiceRecognation/record_mic.py
@@ -1,2 +1,36 @@
 import pyaudio
+import wave
 
+FRAMES_PER_SECOND = 3200
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+
+p = pyaudio.PyAudio()
+
+stream = p.open(
+    format=FORMAT,
+    channels=CHANNELS,
+    rate=RATE,
+    input=True,
+    frames_per_buffer=FRAMES_PER_SECOND
+)
+
+print("start recording")
+
+seconds = 5
+frames = []
+for i in range(0, int(RATE/FRAMES_PER_SECOND*seconds)):
+    data = stream.read(FRAMES_PER_SECOND)
+    frames.append(data)
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+obj = wave.open("output.wav", "wb")
+obj.setnchannels(CHANNELS)
+obj.setsampwidth(p.get_sample_size(FORMAT))
+obj.setframerate(RATE)
+obj.writeframes(b"".join(frames))
+obj.close()
